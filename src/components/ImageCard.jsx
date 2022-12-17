46c30f45e485d7e5f2e2a7ff4719d8c4199d04c7
Mehmet Yanar <ynr.mehmet.60@gmail.com>
2022-12-17 12:07:24 +0100

    Image card updated.

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const ImageCard = ({mydata}) => {
     console.log(mydata);
   return ( mydata
-    .map((item) => <div>
-    <img src={item.src.large} alt="weimar" style={{ width: '200px' }} />
-    <p>{item.photographer}</p> 
-</div>)
+    .map((item) =>
+    <div>
+      <img src={item.src.large} alt="weimar" style={{ width: '200px' }} />
+     <p>{item.photographer}</p> 
+  </div>)
     
   )
 }
